4cc8e271b79c2d8176b70cf458fdb86d806509f5
Ido Ben-Natan <ido.bennatan@gmail.com>
2015-06-19 17:21:32 +0300

    fixing routes resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'companies/show'
-  get 'companies/index'
+  
+  resources :companies, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
